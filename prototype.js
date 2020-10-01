62f0789257704bbbf3dca16db2b1c8cc535103f1
John Doe <AmilitelloDU@gmail.com>
2020-10-01 09:48:50 -0600

    fixed file strucutre

---
+++ prototype.js
@@ -0,0 +1,29 @@
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// passOptions() 
+// random (specialCharacters)
+
+
+// // Generator functions
+// //function getRandomNumber() {
+//     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+// }
+// //function getRandomLower() {
+//     return String.fromCharCode(Math.floor(Math.random() * 25) + 97);
+//     // Math.random generates RANDOM DECIMAL between 0 and 100
+// }
+// function getRandomUpper() {
+//     return String.fromCharCode(Math.floor(Math.random() * 25) + 65);
+// }
+
+// function getRandomSymbol() {
+//     return String.fromCharCode(Math.floor(Math.random(33) * 15) + 47);
+// }
+
+
+// console.log(Math.floor(Math.random() * 26) + 97);
+// console.log(getRandomLower())
+// console.log(getRandomUpper())
+// console.log(getRandomSymbol())
